6755743fc3f35fd01bc57baad4c8ec90369a8c77
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2017-10-29 01:50:17 +0200

    Use contract interface

--- src/main/java/de/informaticum/javabard/api/Formattables.java
+++ src/main/java/de/informaticum/javabard/api/Formattables.java
@@ -1,13 +1,15 @@
 package de.informaticum.javabard.api;
 
+import java.util.Formattable;
+
 public enum Formattables {
     ;
 
-    public static LiteralFormattable literal(final Object anything) {
+    public static Formattable literal(final Object anything) {
         return new LiteralFormattable(anything);
     }
 
-    public static StringFormattable string(final Object anything) {
+    public static Formattable string(final Object anything) {
         return new StringFormattable(anything);
     }
 
