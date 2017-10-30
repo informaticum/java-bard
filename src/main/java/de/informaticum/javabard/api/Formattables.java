3fefa84cd51da80765ebd10257cb2d2741e44822
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2017-10-30 08:36:24 +0100

    Remove LiteralFOrmattable -- that was just unnecessary ;-(

--- src/main/java/de/informaticum/javabard/api/Formattables.java
+++ src/main/java/de/informaticum/javabard/api/Formattables.java
@@ -7,10 +7,6 @@ import de.informaticum.javabard.impl.StringFormattable;
 public enum Formattables {
     ;
 
-    public static Formattable literal(final Object anything) {
-        return new LiteralFormattable(anything);
-    }
-
     public static Formattable string(final Object anything) {
         return new StringFormattable(anything);
     }
