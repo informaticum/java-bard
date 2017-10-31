76706556e0ce7929e85e596983c260961a645d18
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2017-10-31 21:40:21 +0100

    Fail fast principle

--- src/main/java/de/informaticum/javabard/api/FormattableEmitters.java
+++ src/main/java/de/informaticum/javabard/api/FormattableEmitters.java
@@ -1,5 +1,6 @@
 package de.informaticum.javabard.api;
 
+import static java.util.Objects.requireNonNull;
 import java.lang.reflect.Type;
 import java.util.Formattable;
 
@@ -15,18 +16,22 @@ public enum FormattableEmitters {
     }
 
     public static Formattable typeName(final Type type) {
+        requireNonNull(type);
         return (type instanceof Class<?>) ? typeName((Class<?>) type) : (TypeNameEmitter) () -> type;
     }
 
     public static Formattable t(final Type type) {
+        requireNonNull(type);
         return typeName(type);
     }
 
     public static Formattable typeName(final Class<?> type) {
+        requireNonNull(type);
         return (ClassNameEmitter) () -> type;
     }
 
     public static Formattable t(final Class<?> type) {
+        requireNonNull(type);
         return typeName(type);
     }
 
