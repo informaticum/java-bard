2872480cc4b3d27e6087f3e57e6a02bbeaaa86e6
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2017-11-02 01:09:38 +0100

    Add defensive copy and add assert-barriers

--- src/main/java/de/informaticum/javabard/impl/SingleCodeBlock.java
+++ src/main/java/de/informaticum/javabard/impl/SingleCodeBlock.java
@@ -15,20 +15,23 @@ extends AbstractCodeBlock {
     private final Entry<String, Object[]> code;
 
     private SingleCodeBlock(final Entry<String, Object[]> code) {
+        assert code != null;
         this.code = code;
     }
 
     private static final IndentEmitter NO_INDENT = () -> 0;
 
     public SingleCodeBlock(final String format, final Object... args) {
-        this(new SimpleImmutableEntry<>("%s" + format, prepend(NO_INDENT, args)));
+        this(new SimpleImmutableEntry<>("%s" + requireNonNull(format), prepend(NO_INDENT, requireNonNull(args.clone()))));
     }
 
-    private static final Object[] prepend(final Object element, final Object[] from) {
-        final Object[] into = new Object[from.length + 1];
-        into[0] = element;
-        arraycopy(from, 0, into, 1, from.length);
-        return into;
+    private static final Object[] prepend(final IndentEmitter indentor, final Object[] src) {
+        assert indentor != null;
+        assert src != null;
+        final Object[] dest = new Object[src.length + 1];
+        dest[0] = indentor;
+        arraycopy(src, 0, dest, 1, src.length);
+        return dest;
     }
 
     public static final CodeBlock of(final String format, final Object... args) {
@@ -46,6 +49,9 @@ extends AbstractCodeBlock {
     }
 
     private static final Object[] resetIndent(final Object[] args, final int diff) {
+        assert args != null;
+        assert args.length >= 1;
+        assert args[0] instanceof IndentEmitter;
         final Object[] copy = copyOf(args, args.length);
         final int old = ((IndentEmitter) copy[0]).getAsInt();
         copy[0] = (IndentEmitter) () -> max(0, old + diff);
