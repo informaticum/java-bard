3376739bce0526c6d20f9d1a367a0d1c8a46689d
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2017-11-08 18:24:08 +0100

    Include non-null barriers into public API

--- src/main/java/de/informaticum/javabard/api/Code.java
+++ src/main/java/de/informaticum/javabard/api/Code.java
@@ -3,6 +3,7 @@ package de.informaticum.javabard.api;
 import static java.util.Arrays.stream;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Stream.concat;
+import java.util.Objects;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 import de.informaticum.javabard.api.deprecated.Indentable;
@@ -12,9 +13,13 @@ import de.informaticum.javabard.impl.SingleCode;
 public abstract interface Code
 extends Indentable<Code> {
 
+    // fabric methods
+
     public static Code code(final String format, final Object... args) {
         return new SingleCode(requireNonNull(format), requireNonNull(args));
     }
+
+    // instance's methods
 
     public default Code add(final String format, final Object... args) {
         final Code code = code(requireNonNull(format), requireNonNull(args)).indent(this.getIndent());
@@ -22,11 +27,13 @@ extends Indentable<Code> {
     }
 
     public default Code add(final Code... codes) {
-        return new MultiCode(concat(Stream.of(this), stream(requireNonNull(codes))).toArray(Code[]::new));
+        final Code[] combined = concat(Stream.of(this), stream(requireNonNull(codes)).map(Objects::requireNonNull)).toArray(Code[]::new);
+        return new MultiCode(combined);
     }
 
     public default Code add(final Supplier<? extends Code>... codes) {
-        return this.add(stream(codes).map(Supplier::get).toArray(Code[]::new));
+        final Code[] resolved = stream(requireNonNull(codes)).map(Objects::requireNonNull).map(Supplier::get).map(Objects::requireNonNull).toArray(Code[]::new);
+        return this.add(resolved);
     }
 
 }
