c31c780fbb8a639b41957c4c50ca87998d8f24a6
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2017-11-22 22:57:05 +0100

    Integrate deprecated super type

--- src/main/java/de/informaticum/javabard/api/Code.java
+++ src/main/java/de/informaticum/javabard/api/Code.java
@@ -3,10 +3,28 @@ package de.informaticum.javabard.api;
 import static de.informaticum.javabard.util.Util.allNonNull;
 import java.util.Collection;
 import java.util.Locale;
-import de.informaticum.javabard.api.deprecated.Indentable;
 
-public abstract interface Code
-extends Indentable<Code> {
+public abstract interface Code {
+
+    public abstract int getIndent();
+
+    public default Code setIndent(final int indent) {
+        return this.indent(indent - this.getIndent());
+    }
+
+    public default Code clearIndent() {
+        return this.indent(-this.getIndent());
+    }
+
+    public abstract Code indent(final int diff);
+
+    public default Code indent() {
+        return this.indent(+1);
+    }
+
+    public default Code unindent() {
+        return this.indent(-1);
+    }
 
     public abstract Code add(final String format, final Object... args)
     throws IllegalArgumentException;
