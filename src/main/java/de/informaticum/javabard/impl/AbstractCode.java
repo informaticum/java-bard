26acb24d0a9104a98db2c551ed95f79d4ba9410a
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2017-12-01 08:20:49 +0100

    Ups ;-) Add common implementation of code concatenation

--- src/main/java/de/informaticum/javabard/impl/AbstractCode.java
+++ src/main/java/de/informaticum/javabard/impl/AbstractCode.java
@@ -1,11 +1,20 @@
 package de.informaticum.javabard.impl;
 
 import static de.informaticum.javabard.util.Util.nonNull;
+import static java.lang.Math.max;
 import java.util.Locale;
 import de.informaticum.javabard.api.Code;
 
 public abstract class AbstractCode
 implements Code {
+
+    @Override
+    public Code indentNext(final int diff) {
+        final int d = max(diff, -this.getIndent());
+        final Code current = (d < diff) ? this.indent(diff - d) : this;
+        final int nextIndent = current.getIndent() + diff;
+        return new AgglutinateCode.Builder(current, nextIndent).get();
+    }
 
     @Override
     public Code add(final String format, final Object... args)
