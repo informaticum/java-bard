c688e3baadb2464856d93be9f80132410982d199
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2017-12-03 20:29:21 +0100

    Testing the TypeDeclaration API

---
+++ src/test/java/de/informaticum/javabard/api/TypeDeclarationTests.java
@@ -0,0 +1,41 @@
+package de.informaticum.javabard.api;
+
+import static de.informaticum.javabard.impl.SimpleTypeDeclaration.declare;
+import static java.lang.String.format;
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.hasToString;
+import static org.junit.Assert.assertThat;
+import java.util.List;
+import java.util.function.Function;
+import de.informaticum.javabard.impl.SimpleTypeDeclaration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class TypeDeclarationTests {
+
+    @Parameters
+    public static List<Function<String, TypeDeclaration>> codeFactories() {
+        final Function<String, TypeDeclaration> dec = (n) -> declare(n);
+        final Function<String, TypeDeclaration> std = (n) -> new SimpleTypeDeclaration().setName(n);
+        return asList(dec, std);
+    }
+
+    @Parameter(0)
+    public Function<String, TypeDeclaration> factory;
+
+    private TypeDeclaration make(final String name) {
+        return this.factory.apply(name);
+    }
+
+    @Test
+    public void testSimpleClassDeclaration()
+    throws Exception {
+        final TypeDeclaration def = this.make("FooBar");
+        assertThat(def, hasToString(format("class FooBar {%n}%n")));
+    }
+
+}
