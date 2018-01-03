fcf2c87d851f0551e7e598ab4dc5ddb78bc29776
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2018-01-03 21:04:58 +0100

    Move fabric methods into API component

--- src/main/java/de/informaticum/javabard/api/Code.java
+++ src/main/java/de/informaticum/javabard/api/Code.java
@@ -1,10 +1,13 @@
 package de.informaticum.javabard.api;
 
 import static de.informaticum.javabard.util.Util.allNonNull;
+import static de.informaticum.javabard.util.Util.nonNull;
 import static java.lang.Math.max;
 import static java.util.Arrays.asList;
 import java.util.IllegalFormatException;
 import java.util.Locale;
+import de.informaticum.javabard.impl.MultiCode;
+import de.informaticum.javabard.impl.SingleCode;
 
 public abstract interface Code {
 
@@ -52,5 +55,52 @@ public abstract interface Code {
     @Override
     public abstract String toString()
     throws IllegalFormatException;
+
+    // fabric methods
+
+    static Code emptyCode() {
+        return new MultiCode.Builder().get();
+    }
+
+    static Code code(final String format, final Object... args)
+    throws IllegalArgumentException {
+        nonNull(format);
+        nonNull(args);
+        return new SingleCode.Builder(format, args).get();
+    }
+
+    static Code code(final Locale locale, final String format, final Object... args)
+    throws IllegalArgumentException {
+        nonNull(locale);
+        nonNull(format);
+        nonNull(args);
+        return new SingleCode.Builder(format, args).setLocale(locale).get();
+    }
+
+    static Code combine(final Iterable<? extends Code> codes)
+    throws IllegalArgumentException {
+        allNonNull(codes);
+        return new MultiCode.Builder().add(codes).get();
+    }
+
+    static Code combine(final Code... codes)
+    throws IllegalArgumentException {
+        allNonNull(codes);
+        return new MultiCode.Builder(codes).get();
+    }
+
+    static Code combine(final Code code, final Iterable<? extends Code> codes)
+    throws IllegalArgumentException {
+        nonNull(code);
+        allNonNull(codes);
+        return new MultiCode.Builder(code).add(codes).get();
+    }
+
+    static Code combine(final Code code, final Code[] codes)
+    throws IllegalArgumentException {
+        nonNull(code);
+        allNonNull(codes);
+        return new MultiCode.Builder(code).add(codes).get();
+    }
 
 }
--- src/main/java/de/informaticum/javabard/impl/AbstractCode.java
+++ src/main/java/de/informaticum/javabard/impl/AbstractCode.java
@@ -1,6 +1,5 @@
 package de.informaticum.javabard.impl;
 
-import static de.informaticum.javabard.util.Util.allNonNull;
 import static de.informaticum.javabard.util.Util.nonNull;
 import java.util.Locale;
 import de.informaticum.javabard.api.Code;
@@ -30,53 +29,6 @@ implements Code {
     throws IllegalArgumentException {
         nonNull(code);
         return new MultiCode.Builder(this, code.indentBy(this.getIndent())).get();
-    }
-
-    // fabric methods
-
-    public static final Code emptyCode() {
-        return new MultiCode.Builder().get();
-    }
-
-    public static final Code code(final String format, final Object... args)
-    throws IllegalArgumentException {
-        nonNull(format);
-        nonNull(args);
-        return new SingleCode.Builder(format, args).get();
-    }
-
-    public static final Code code(final Locale locale, final String format, final Object... args)
-    throws IllegalArgumentException {
-        nonNull(locale);
-        nonNull(format);
-        nonNull(args);
-        return new SingleCode.Builder(format, args).setLocale(locale).get();
-    }
-
-    public static final Code combine(final Iterable<? extends Code> codes)
-    throws IllegalArgumentException {
-        allNonNull(codes);
-        return new MultiCode.Builder().add(codes).get();
-    }
-
-    public static final Code combine(final Code... codes)
-    throws IllegalArgumentException {
-        allNonNull(codes);
-        return new MultiCode.Builder(codes).get();
-    }
-
-    public static final Code combine(final Code code, final Iterable<? extends Code> codes)
-    throws IllegalArgumentException {
-        nonNull(code);
-        allNonNull(codes);
-        return new MultiCode.Builder(code).add(codes).get();
-    }
-
-    public static final Code combine(final Code code, final Code[] codes)
-    throws IllegalArgumentException {
-        nonNull(code);
-        allNonNull(codes);
-        return new MultiCode.Builder(code).add(codes).get();
     }
 
 }
